0f4b2725452e812d6615f9c27c2274a805c849a9
DesaleRohit <rohitdesale2005@gmail.com>
2024-07-26 17:28:24 +0530

    [ADEDD]:Rohit -PHP program for Arithmetic Operation

---
+++ Arithmetic.php
@@ -0,0 +1,32 @@
+<?php
+
+$a = 20;
+$b = 5;
+$c = 0;
+
+//Addition 
+
+$c = $a + $b;
+echo "Addition=" . $c;
+
+//Subtraction
+
+$c = $a - $b;
+echo "<br><br>Subtraction=" . $c;
+
+//Multiplication
+
+$c = $a * $b;
+echo "<br><br>Multiplication=" . $c;
+
+//Division
+
+$c = $a / $b;
+echo "<br><br>Division=" . $c;
+
+//Remainder
+
+$c = $a % $b;
+echo "<br><br>Remainder=" . $c;
+
+?>
