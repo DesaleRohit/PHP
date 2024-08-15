f34dce15e427666f4b4052c32c6602ec47392d64
DesaleRohit <rohitdesale2005@gmail.com>
2024-08-15 12:24:35 +0530

    [ADDED]:Rohit - To find Prime Number using PHP

---
+++ if-else-codes/primeno.php
@@ -0,0 +1,26 @@
+<?php
+    $n = $_POST["number"];
+    if($n==1)
+    {
+        $flag=1;
+    }
+    else
+    {
+        for($i=2; $i<=$n-1; $i++)
+        {
+            if($n % $i == 0)
+            {
+               $flag=0;
+               break; 
+            }
+        }
+    }
+   if($flag==1)
+    {
+        echo "The Number is Prime";
+    }
+    else
+    {
+        echo "The Number is Not-Prime";
+    }
+?>
