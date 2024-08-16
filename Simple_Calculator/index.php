72fc8d36dba9f63a5905b73a0bb426d4d2c5e4b9
DesaleRohit <rohitdesale2005@gmail.com>
2024-08-16 22:22:30 +0530

    [Modified]:Rohit - Added GET method in code

--- Simple_Calculator/index.php
+++ Simple_Calculator/index.php
@@ -4,10 +4,10 @@
 </head>
 
 <body>
-    <form action="index.php" method="POST" target="_self">
-        <label>Num 1:</label><input type="text" id="n1" name="n1" /><br>
+    <form action="index.php" method="GET">
+        <label>Num 1:</label><input type="text" id="n1" name="n1"/><br>
         <label>Operator: </label><input type="text" id="op" name="op" /><br>
-        <label>Num 2:</label><input type="text" id="n2" name="n2" /><br>
+        <label>Num 2:</label><input type="text" id="n2" name="n2"/><br>
         <input type="submit" value="Calculate">
     </form>
 </body>
@@ -16,26 +16,25 @@
 
 <?php
 
-$n1 = $_POST["n1"];
-$op = $_POST["op"];
-$n2 = $_POST["n2"];
+$n1 = $_GET["n1"];
+$op = $_GET["op"];
+$n2 = $_GET["n2"];
 
-if ($op == "+") 
-{
-    $ans = $n1 + $n2;
-} 
-else if ($op == "-") 
-{
-    $ans = $n1 - $n2;
-} 
-else if ($op == "*") 
-{
-    $ans = $n1 * $n2;
-} 
-else if ($op == "/") 
-{
-    $ans = $n1 / $n2;
-}
+    if ($op == "+") 
+    {
+        $ans = $n1 + $n2;
+    }
+    else if ($op == "-") 
+    {
+        $ans = $n1 - $n2;
+    } 
+    else if ($op == "*") 
+    {
+        $ans = $n1 * $n2;
+    } 
+    else if ($op == "/")
+    {
+        $ans = $n1 / $n2;
+    }
 
-echo $ans;
 ?>
