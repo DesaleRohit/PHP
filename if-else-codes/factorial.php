e27ea0d9ae476eeed241887e322832855f1ff63e
DesaleRohit <rohitdesale2005@gmail.com>
2024-09-07 16:50:43 +0530

    [Modified]:Rohit- Added Backgroung color and font size

--- if-else-codes/factorial.php
+++ if-else-codes/factorial.php
@@ -8,6 +8,23 @@ for ($i = 1; $i <= $n; $i++)
  {
     $fact = $fact * $i;
  }
-echo "Factorial of $n is: $fact";
+echo "<p>Factorial of $n is: $fact</p>";
 
 ?>
+    <head>
+        <style>
+            body{
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            background-color: white;
+            color: black;
+            font-size: 5px;
+            }
+           
+            p{
+              font-size: 55px; 
+              color: black;
+            }
+        </style>
