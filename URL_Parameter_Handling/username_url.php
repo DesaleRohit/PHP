38bec39610b9162ae3a092c78035a83a0801e0aa
DesaleRohit <rohitdesale2005@gmail.com>
2024-09-08 15:12:38 +0530

    [ADDED]:Rohit - Variable passing through URl using Request method

---
+++ URL_Parameter_Handling/username_url.php
@@ -0,0 +1,14 @@
+<?php
+    $name = $_REQUEST["name"];
+    $email = $_REQUEST["email"];
+    $pass = $_REQUEST["pass"];
+    $mobile = $_REQUEST["mobile"];
+
+    echo "<p>Name: $name</p>";
+    echo "<p>Email: $email</p>";
+    echo "<p>Password: $pass</p>";
+    echo "<p>Mobile: $mobile</p>";
+
+?>
+<!-- name=Rohit&&email=rohitdesale2005@gmail.com&&pass=123456&&mobile=7841859029 -->
+
