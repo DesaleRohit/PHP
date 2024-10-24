d2a7e8fb3f1838fe60a340c0430c812b4d4873a0
DesaleRohit <rohitdesale2005@gmail.com>
2024-10-24 19:27:35 +0530

    [ADDED]:Rohit - implode function demo

---
+++ String Functions/implode_demo.php
@@ -0,0 +1,8 @@
+<?php
+    $arr = array("Wellcome", "to", "my", "world");
+    echo implode(" ", $arr),"<br>";
+    echo implode("-", $arr),"<br>";
+    echo implode("+", $arr),"<br>";
+    echo implode("->", $arr),"<br>";
+     
+?>
