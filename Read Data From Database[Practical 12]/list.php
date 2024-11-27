d1ba4f1cbc48a478c0627cb765f38344ee9c97db
DesaleRohit <rohitdesale2005@gmail.com>
2024-11-27 16:37:59 +0530

    [Modified]:Rohit -Added new Array feature

--- Read Data From Database[Practical 12]/list.php
+++ Read Data From Database[Practical 12]/list.php
@@ -1,26 +1,27 @@
-<?
-    $db = mysqli_connect("localhost", "root", "root", "registerdb");
+<?php
+$db = mysqli_connect("localhost", "root", "root", "registerdb");
 
-    $result = mysqli_query($db, "SELECT * FROM studentinfo");
+$result = mysqli_query($db, "SELECT * FROM studentinfo");
 
-    echo "<h1>List of Student</h1>";
-    echo "<table border=1>
-     <tr> 
-        <th>ID</th>
-        <th>Name</th>
-        <th>Mobile</th>
-        <th>Email</th>
-     </tr>";
-
-     while($row = $result->fetch_assoc())
-     {
-         echo "<tr>";
-         echo "<td>" . $row["id"] . "</td>";
-         echo "<td>" . $row["name"] . "</td>";
-         echo "<td>" . $row["mobile"] . "</td>";
-         echo "<td>" . $row["email"] . "</td>";
-         echo "</tr>";
-     }
-
-     echo "</table>";
-?>
+echo "<h1>List of Student</h1>";
+echo "<table border=1>
+ <tr> 
+    <th>ID</th>
+    <th>Name</th>
+    <th>Mobile</th>
+    <th>Email</th>
+ </tr>";
+if ($result->num_rows > 0) {
+    // output data of each row
+    while ($row = $result->fetch_assoc()) {
+        echo "<tr>";
+        echo "<td>" . $row['id'] . "</td>";
+        echo "<td>" . $row['name'] . "</td>";
+        echo "<td>" . $row['mobile'] . "</td>";
+        echo "<td>" . $row['email'] . "</td>";
+        echo "</tr>";
+    }
+} else {
+    echo "0 results";
+}
+echo "</table>";
